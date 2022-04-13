117b3e89efb7b685c85770f0b50874d39f409a6d
Geovane Oliveira <olivira.5geovane@outlook.com>
2022-04-13 18:38:42 -0300

    projeto quase feito

--- jscript.js
+++ jscript.js
@@ -1,11 +1,63 @@
+
+//a posição do parâmetro no array é considerado a posição do atleta 
+
+// Daniel possui bônus, sempre vai subir uma posição
+
 // function positions (firstPlace, secondePlace, lastPlace){
-//    const placing = [firstPlace, secondePlace, lastPlace]
-// //a posição do parâmetro no array é considerado a posição do atleta 
-//     let athletes = ['Daniel', 'Rafael', 'Manoel']
-//     // Daniel possui bônus, sempre vai subir uma posição
+
+//     const placing = [firstPlace, secondePlace, lastPlace]
+
+//     if(placing[0] === 'Daniel'){
+//         console.log(placing)
+//     } else {
+//         for (let i = 0; i < placing.length; i++){
+//             if (placing[i] === 'Daniel'){               
+//                 placing[i] = placing[i - 1]
+//                 placing[i - 1] = 'Daniel'
+//             }
+//         }
+//         console.log(placing)
+//     }
 // }
+// positions('Manoel', 'Daniel', 'Rafael')
 
-let nomeDosCompetidore = ['Maria', 'Marcos', 'João', 'Roberto', 'Carlos']
-let posicoesDoPodio = [1, 2, 3, 4, 5]
 
-console.log(posicoesDoPodio[nomeDosCompetidore])
+
+
+
+
+
+
+
+
+function positions (firstPlace, secondePlace, lastPlace){
+
+    const placing = [firstPlace, secondePlace, lastPlace]
+
+    if(placing[0] === 'Daniel'){
+        console.log(`1º - Daniel 2º - ${secondePlace} 3º - ${lastPlace}`)
+    } 
+    else if (placing[1] === 'Daniel'){
+        console.log(`1º - Daniel 2º - ${firstPlace} 3º - ${lastPlace}`)
+    }
+    else if(placing[2] === 'Daniel'){
+        console.log(`1º - ${firstPlace} 2º - Daniel 3º - ${secondePlace}`)
+    }
+ }
+positions('Manoel', 'Daniel', 'Rafael')
+    
+    // if (('Rafael' == placing[0]) && ('Daniel' == placing[1])){
+    //     console.log('1º Colocado é Daniel')
+    // } 
+    // else if (('Rafael' == placing[1]) && ('Daniel' == placing[2])){
+    //     console.log('1º Colocado é Manoel')
+    // }
+    //  else if (('Manoel' == placing[0]) && ('Daniel' == placing[1])){
+    //     console.log('1º Colocado é Daniel')
+    // } 
+    // else if (('Manoel' == placing[1]) && ('Daniel' == placing[2])){
+    //     console.log('1º Colocado é Rafael')
+    // }
+    //  else{
+    //     console.log('1º Colocado é Daniel')
+    // }
