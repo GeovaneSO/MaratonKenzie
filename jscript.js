9ac20a62e6808ba5c931fdcb298d32a0660fe33c
Geovane Oliveira <olivira.5geovane@outlook.com>
2022-04-13 10:55:03 -0300

    iniciando projeto

---
+++ jscript.js
@@ -0,0 +1,11 @@
+// function positions (firstPlace, secondePlace, lastPlace){
+//    const placing = [firstPlace, secondePlace, lastPlace]
+// //a posição do parâmetro no array é considerado a posição do atleta 
+//     let athletes = ['Daniel', 'Rafael', 'Manoel']
+//     // Daniel possui bônus, sempre vai subir uma posição
+// }
+
+let nomeDosCompetidore = ['Maria', 'Marcos', 'João', 'Roberto', 'Carlos']
+let posicoesDoPodio = [1, 2, 3, 4, 5]
+
+console.log(posicoesDoPodio[nomeDosCompetidore])
