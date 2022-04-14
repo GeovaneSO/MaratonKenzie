812de781055ef9c4d4d943fbb519e6cb8eb307e3
GeovaneSO <103089710+GeovaneSO@users.noreply.github.com>
2022-04-14 02:00:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ function positions (firstPlace, secondePlace, lastPlace){
     const placing = [firstPlace, secondePlace, lastPlace]
 
     if (placing[0] === 'Daniel'){
-        console.log(`1º - Daniel 2º - ${secondePlace} 3º - ${lastPlace}`)
+        console.log(`1º - Daniel, 2º - ${secondePlace} e 3º - ${lastPlace}`)
     } 
     else if (placing[1] === 'Daniel'){
-        console.log(`1º - Daniel 2º - ${firstPlace} 3º - ${lastPlace}`)
+        console.log(`1º - Daniel, 2º - ${firstPlace} e 3º - ${lastPlace}`)
     }
     else if (placing[2] === 'Daniel'){
-        console.log(`1º - ${firstPlace} 2º - Daniel 3º - ${secondePlace}`)
+        console.log(`1º - ${firstPlace}, 2º - Daniel e 3º - ${secondePlace}`)
     }
  }
 positions('Manoel', 'Rafael', 'Daniel')
